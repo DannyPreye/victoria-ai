31f43955f24d96771d8a5bf01ddf474da7bbdc78
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-10-09 17:28:12 +0100

    fixed question mark in create cover letter page

--- src/components/pages/dashboard/create-cover-letter/WhichRadioButton.tsx
+++ src/components/pages/dashboard/create-cover-letter/WhichRadioButton.tsx
@@ -33,7 +33,7 @@ export const WhichRadioButton = ({
                     className='text-gray-900  text-[14px] gap-2 font-[500] flex items-center'
                 >
                     <span> {title}</span>
-                    <span className='h-[16px] w-[16px] text-[8px] leading-[120%] font-[500] grid place-items-center bg-base-primary-green rounded-full text-white '>
+                    <span className='h-[16px]  w-[16px] flex-shrink-0 text-[8px] leading-[120%] font-[500] grid place-items-center bg-base-primary-green rounded-full text-white '>
                         ?
                     </span>
                 </label>
