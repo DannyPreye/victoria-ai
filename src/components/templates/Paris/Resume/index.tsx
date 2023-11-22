28b0a59f36cb4af5c6e4ec9a58aebf7bbb77fdea
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-11-22 18:34:25 +0100

    finished the coverletter preview for paris template

--- src/components/templates/Paris/Resume/index.tsx
+++ src/components/templates/Paris/Resume/index.tsx
@@ -1,10 +1,11 @@
 import { GoDotFill } from "react-icons/go";
 import React from "react";
 
-import Image from "next/image";
 import { ResumeSectionType, TemplateDocumentType } from "@/types/document";
-import { Contact } from "../../components/Contact";
+
 import { convertObjectToArray } from "@/utils/functions/converNumericObjectToArray";
+import { ParisHeading } from "../components/ParisHeading.paris";
+import { ContactSection } from "../components/ContactSection";
 
 interface Props {
     document: TemplateDocumentType;
@@ -17,7 +18,6 @@ const ParisResume = ({ document }: Props) => {
         ?.split(" ")[0];
     const lastName = resume?.sections?.heading?.username?.trim().split(" ")[1];
 
-    console.log(resume.sections);
     const education = resume.sections.education
         ? convertObjectToArray(resume.sections.education)
         : [];
@@ -30,133 +30,66 @@ const ParisResume = ({ document }: Props) => {
         : [];
 
     return (
-        <div className='flex min-h-screen'>
-            <div
-                style={{ borderRight: `5px solid black` }}
-                className='w-[40%] bg-[#d7d5d5] p-12 flex flex-col gap-[150px]'
+        <div className='flex min-h-screen relative overflow-hidden'>
+            <ParisHeading
+                username={resume?.sections?.heading?.username}
+                professionalTitle={resume?.sections?.heading?.professionalTitle}
+            />
+            {/* Contact Section */}
+            <ContactSection
+                color={document.color}
+                addProfilePicture={document.addProfilePicture}
+                profilePicture={document.profilePicture}
+                heading={resume.sections?.heading}
             >
-                <div className=' flex flex-col gap-8'>
-                    {/* Profile Picture */}
-                    {document.addProfilePicture && (
-                        <div className='w-[200px] relative overflow-hidden h-[200px] border-gray-500 border-[5px] rounded-full bg-gray-100 mx-auto'>
-                            <Image
-                                src={
-                                    document.profilePicture.data?.attributes
-                                        ?.url as string
-                                }
-                                fill
-                                alt=''
-                                className='object-fit'
-                            />
-                        </div>
-                    )}
-                    {/* Contact */}
-                    <div className={``}>
-                        <h2 className='text-gray-600 uppercase text-2xl font-semibold'>
-                            Contact
-                        </h2>
-                        <Contact
-                            contact={resume.sections.heading.contact}
-                            color={document.color}
-                        />
-                    </div>
-                </div>
                 {/* Education */}
-                <div className='flex flex-col gap-3'>
-                    <h2 className='text-gray-600 uppercase text-2xl font-semibold'>
-                        Education
+                <div className={``}>
+                    <h2 className='text-gray-500 border-black uppercase mx-auto w-[80%] text-center border-b-[1px] text-2xl font-semibold'>
+                        Eductaion
                     </h2>
-                    <div className='grid gap-4'>
+                    <div className='flex flex-col items-center w-[80%] mx-auto'>
                         {education.map((item, id) => (
-                            <div key={id}>
-                                <p className='font-bold text-[1.2rem] '>
+                            <div
+                                className='flex flex-col items-center'
+                                key={id}
+                            >
+                                <h3 className='font-bold text-gray-700 text-[1.2rem] '>
                                     <span>{item?.school}</span> |{" "}
                                     <span>{item?.location || "Location"}</span>
-                                </p>
+                                </h3>
                                 <p className='text-gray-500 font-semibold'>
                                     {item?.courseOfStudy}
                                 </p>
-                                <p>{item?.endYear}</p>
+                                <p className='text-gray-800'>{item?.endYear}</p>
                             </div>
                         ))}
                     </div>
                 </div>
-                {/* Skill */}
-                <div className='flex flex-col gap-3'>
-                    <h2 className='text-gray-600 uppercase text-2xl font-semibold'>
+
+                {/*SKILLS  */}
+                <div className={``}>
+                    <h2 className='text-gray-500 border-black uppercase mx-auto w-[80%] text-center border-b-[1px] text-2xl font-semibold'>
                         Skills
                     </h2>
-                    <div className='grid gap-4'>
-                        {skills.map((skill) => (
-                            <div
-                                className='flex items-center gap-8 text-gray-500'
-                                key={skill}
-                            >
-                                <GoDotFill />
-                                <span>{skill}</span>
-                            </div>
+                    <div className='flex flex-col gap-2 items-center w-[80%] mt-4 mx-auto'>
+                        {skills.map((skill, id) => (
+                            <p key={id}>{skill}</p>
                         ))}
                     </div>
                 </div>
-            </div>
-            <div className='w-[70%] bg-gray-100 px-12 py-16 '>
-                {/* Heading */}
-                <div className='py-5 border-b-[5px] border-gray-400 grid gap-4'>
-                    <h1
-                        style={{ color: "black" }}
-                        className={`font-bold text-4xl uppercase  flex gap-2 `}
-                    >
-                        <span>{firstName}</span>
-                        <span className='text-gray-500'>{lastName}</span>
-                    </h1>
-                    <p>{resume?.sections.heading?.professionalTitle}</p>
-                </div>
+            </ContactSection>
 
-                {/* Professional Summary */}
-                <div className='my-10 grid gap-3'>
-                    <MainBodyHeading title='Profile' />
-                    <p className='text-justify text-gray-500'>
+            {/* Main Body Section  */}
+            <section className='w-[70%] bg-gray-100 px-12 py-16 pt-[300px]'>
+                <div className={``}>
+                    <h2 className='text-gray-500  border-black uppercase mx-auto mt-5  text-center border-b-[1px] text-2xl font-semibold'>
+                        About Me
+                    </h2>
+                    <div className='flex flex-col gap-2 text-gray-500 leading-[140%] text-justify items-center  mt-4 mx-auto'>
                         {resume.sections.professionalSummary}
-                    </p>
-                </div>
-
-                {/* Work Experience */}
-                <div className='my-10 grid gap-3'>
-                    <MainBodyHeading title='Work Experience' />
-
-                    <div className=''>
-                        {workExperience.map((experience, id) => (
-                            <div
-                                className='grid gap-4'
-                                key={`experience_${id}`}
-                            >
-                                <div className='flex justify-between items-center font-bold text-gray-600'>
-                                    <span className=''>
-                                        {experience?.jobTitle || "Job Title"}
-                                    </span>
-                                    <span>
-                                        {experience?.startYear} -{" "}
-                                        {experience?.endYear || "Present"}
-                                    </span>
-                                </div>
-                                <div className='pl-10 mt-5 text-gray-500'>
-                                    {experience?.achivements?.map(
-                                        (achievement: any) => (
-                                            <div
-                                                key={achievement}
-                                                className='flex items-center gap-2'
-                                            >
-                                                <GoDotFill />{" "}
-                                                <span>{achievement}</span>
-                                            </div>
-                                        )
-                                    )}
-                                </div>
-                            </div>
-                        ))}
                     </div>
                 </div>
-            </div>
+            </section>
         </div>
     );
 };
